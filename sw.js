b82cf52f2f760a7ab007cc2cfe40c11f6f1c29d5
simaehlmann-cloud <simaehlmann@gmail.com>
2026-09-19 17:08:47 +0200

    Update cache version to 1.23.2

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    APP_VERSION in der index.html: Wer die Version anhebt, ohne diese Zeile
    nachzuziehen, liefert Rueckkehrern sonst weiter die alte Fassung aus
    dem Cache aus. build-lite.sh setzt hier 'fairmixlite-' ein. */
-const CACHE = 'fairmix-1.23.1';
+const CACHE = 'fairmix-1.23.2';
 
 const ASSETS = [
   './',
